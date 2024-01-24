d2c3064398d48ad7014d8506a29b1ea3e00fa270
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-24 22:52:19 +0600

    header fixed

--- src/components/ui/layouts/Navbar.tsx
+++ src/components/ui/layouts/Navbar.tsx
@@ -1,9 +1,15 @@
+import { motion } from "framer-motion";
 import { NavLink } from "react-router-dom";
 import { Button } from "../button";
 
 const Navbar = () => {
   return (
-    <header className="h-16">
+    <motion.header
+      className="h-16 fixed w-full bg-white z-[999]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1, delay: 1 }}
+    >
       <nav className="w-full h-full max-w-[1230px] px-[20px] mx-auto flex justify-between items-center">
         <span className="text-3xl ">iRepair</span>
         <ul className="space-x-5">
@@ -15,7 +21,7 @@ const Navbar = () => {
           </Button>
         </ul>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
       transition: {
         duration: 0.5,
         staggerChildren: 0.25,
-        delay: 0.2,
+        delayChildren: 1,
       },
     },
   };
@@ -22,7 +22,7 @@ const HeroSection = () => {
       opacity: 1,
       y: 0,
       transition: {
-        duration: 0.7,
+        duration: 1,
         type: "spring",
         bounce: 0.5,
       },
@@ -30,10 +30,13 @@ const HeroSection = () => {
   };
 
   const laptop = {
-    initial: { y: 0 },
+    initial: { y: 0, rotate: 0, scale: 5 },
     animate: {
+      rotate: -30,
+      scale: 1,
       y: 20,
       transition: {
+        duration: 1,
         y: {
           duration: 2,
           repeat: Infinity,
@@ -45,43 +48,45 @@ const HeroSection = () => {
   };
 
   return (
-    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
-      <motion.div variants={intro} initial="hidden" animate="visible">
-        <motion.h1
-          className="text-5xl md:text-8xl font-bold text-nowrap"
-          variants={introChildren}
-        >
-          <span className="text-gray">Don't worry,</span>
-          <br />
-          <span>We will fix it.</span>
-        </motion.h1>
-        <motion.p
-          className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
-          variants={introChildren}
-        >
-          Welcome to
-          <span className=" text-primary-foreground font-semibold">
-            iRepair
-          </span>
-          , your one stop place for all kinds of{" "}
-          <span className=" text-primary-foreground font-semibold">
-            MacBook repairs{" "}
-          </span>
-          and diagnostics
-        </motion.p>
-        <motion.div variants={introChildren}>
-          <Button>Book a service</Button>
+    <div className=" overflow-hidden">
+      <Container className="h-screen pt-16 grid grid-cols-1 lg:grid-cols-2 place-content-center">
+        <motion.div variants={intro} initial="hidden" animate="visible">
+          <motion.h1
+            className="text-5xl md:text-8xl font-bold text-nowrap"
+            variants={introChildren}
+          >
+            <span className="text-gray">Don't worry,</span>
+            <br />
+            <span>We'll fix it.</span>
+          </motion.h1>
+          <motion.p
+            className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+            variants={introChildren}
+          >
+            Welcome to
+            <span className=" text-primary-foreground font-semibold">
+              iRepair
+            </span>
+            , your one stop place for all kinds of{" "}
+            <span className=" text-primary-foreground font-semibold">
+              MacBook repairs{" "}
+            </span>
+            and diagnostics
+          </motion.p>
+          <motion.div variants={introChildren}>
+            <Button>Book a service</Button>
+          </motion.div>
         </motion.div>
-      </motion.div>
-      <motion.div
-        className="mt-10 w-3/4 lg:w-full mx-auto"
-        variants={laptop}
-        initial="initial"
-        animate="animate"
-      >
-        <img className="-rotate-[35deg] h-[95%] object-contain" src={macBook} />
-      </motion.div>
-    </Container>
+        <motion.div
+          className="mt-10 w-3/4 lg:w-full mx-auto"
+          variants={laptop}
+          initial="initial"
+          animate="animate"
+        >
+          <img className=" h-[95%] object-contain" src={macBook} />
+        </motion.div>
+      </Container>
+    </div>
   );
 };
 
