9a5582bef37a8b19866f8b1a3613dacb6a0bb0cb
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-24 20:41:17 +0600

    text animation added

--- src/pages/home/HeroSection.tsx
+++ src/pages/home/HeroSection.tsx
@@ -1,17 +1,48 @@
 import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 import macBook from "../../assets/images/macbook-exposed.png";
 
 const HeroSection = () => {
+  const intro = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.5,
+        staggerChildren: 0.25,
+        delay: 0.2,
+      },
+    },
+  };
+
+  const introChildren = {
+    hidden: { opacity: 0, y: -200 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.7,
+        type: "spring",
+        bounce: 0.5,
+      },
+    },
+  };
   return (
     <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
-      <div>
-        <h1 className="text-5xl md:text-8xl font-bold text-nowrap">
+      <motion.div variants={intro} initial="hidden" animate="visible">
+        <motion.h1
+          className="text-5xl md:text-8xl font-bold text-nowrap"
+          variants={introChildren}
+        >
           <span className="text-gray">Don't worry,</span>
           <br />
           <span>We will fix it.</span>
-        </h1>
-        <p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+        </motion.h1>
+        <motion.p
+          className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg"
+          variants={introChildren}
+        >
           Welcome to
           <span className=" text-primary-foreground font-semibold">
             iRepair
@@ -21,9 +52,11 @@ const HeroSection = () => {
             MacBook repairs{" "}
           </span>
           and diagnostics
-        </p>
-        <Button>Book a service</Button>
-      </div>
+        </motion.p>
+        <motion.div variants={introChildren}>
+          <Button>Book a service</Button>
+        </motion.div>
+      </motion.div>
       <div className="mt-10 w-3/4 lg:w-full mx-auto">
         <img className="-rotate-[35deg] h-[95%] object-contain" src={macBook} />
       </div>
