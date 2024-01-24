5168d1daa0ba91e4dcdcbf21c611ae814d7113fc
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-24 20:14:47 +0600

    responsive hero

--- src/components/ui/layouts/Navbar.tsx
+++ src/components/ui/layouts/Navbar.tsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Button } from "../button";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </div>
+    <header className="h-16">
+      <nav className="w-full h-full max-w-[1230px] px-[20px] mx-auto flex justify-between items-center">
+        <span className="text-3xl ">iRepair</span>
+        <ul className="space-x-5">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+
+          <Button>
+            <NavLink to="/login">Login</NavLink>
+          </Button>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
---
+++ src/pages/home/HeroSection.tsx
@@ -0,0 +1,34 @@
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+import macBook from "../../assets/images/macbook-exposed.png";
+
+const HeroSection = () => {
+  return (
+    <Container className="h-[calc(100vh-64px)] grid grid-cols-1 lg:grid-cols-2 place-content-center">
+      <div>
+        <h1 className="text-5xl md:text-8xl font-bold text-nowrap">
+          <span className="text-gray">Don't worry,</span>
+          <br />
+          <span>We will fix it.</span>
+        </h1>
+        <p className="text-dark-gray max-w-[50ch] mt-10 mb-5 text-lg">
+          Welcome to
+          <span className=" text-primary-foreground font-semibold">
+            iRepair
+          </span>
+          , your one stop place for all kinds of{" "}
+          <span className=" text-primary-foreground font-semibold">
+            MacBook repairs{" "}
+          </span>
+          and diagnostics
+        </p>
+        <Button>Book a service</Button>
+      </div>
+      <div className="mt-10 w-3/4 lg:w-full mx-auto">
+        <img className="-rotate-[35deg] h-[95%] object-contain" src={macBook} />
+      </div>
+    </Container>
+  );
+};
+
+export default HeroSection;
