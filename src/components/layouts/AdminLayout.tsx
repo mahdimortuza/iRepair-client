2c81e09dc8f3144a5eba02303153311ac6ec4500
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-25 23:59:53 +0600

    col span added

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -5,7 +5,7 @@ const AdminLayout = () => {
   return (
     <div className="grid grid-cols-12">
       <Sidebar />
-      <div className="col-span-12">
+      <div className="col-span-10">
         <Outlet />
       </div>
     </div>
