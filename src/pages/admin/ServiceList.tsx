798981a22c02e06c3ced074424e541d64d13fae2
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-26 11:55:54 +0600

    data fetching

--- src/pages/admin/ServiceList.tsx
+++ src/pages/admin/ServiceList.tsx
@@ -1,5 +1,22 @@
+import { getServices } from "@/api/admin/service/service.api";
+import { useQuery } from "@tanstack/react-query";
+
 const ServiceList = () => {
-  return <div>ServiceList</div>;
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["services"],
+    queryFn: getServices,
+  });
+  if (isLoading) {
+    return <h2>loading</h2>;
+  }
+  console.log({ isLoading, data });
+  return (
+    <div>
+      {data.data.map((item) => (
+        <h1 key={item.name}>{item.name}</h1>
+      ))}
+    </div>
+  );
 };
 
 export default ServiceList;
